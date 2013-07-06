de6d680344f65928b225eb18011101ec845f82df
startupmooc <startupmooc@gmail.com>
2013-07-06 11:20:12 -0400

    Updated web.js

--- web.js
+++ web.js
@@ -3,9 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var b = fs.readFileSync('index.html')
-  var s = b.toString('utf-8')
-  response.send(s);
+  var fs = require("fs");
+  var data = fs.readFileSync('index.html', 'utf8');
+  //var s = b.toString('utf-8')
+  response.send(data);
 });
 
 var port = process.env.PORT || 5000;
